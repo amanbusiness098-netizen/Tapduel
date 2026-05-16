9ae38cc63f6d2b0e15bf9eb71938191bde02728d
Aman <amanbusiness098@gmail.com>
2026-05-16 20:23:27 +0530

    Add install app button

--- script.js
+++ script.js
@@ -345,3 +345,22 @@ copyRoomBtn.addEventListener("click", () => {
   navigator.clipboard.writeText(inviteText);
   alert("Room link copied!");
 });
+
+let deferredPrompt;
+const installBtn = document.getElementById("installBtn");
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  installBtn.style.display = "block";
+});
+
+installBtn.addEventListener("click", async () => {
+  if (!deferredPrompt) return;
+
+  deferredPrompt.prompt();
+  await deferredPrompt.userChoice;
+
+  deferredPrompt = null;
+  installBtn.style.display = "none";
+});
